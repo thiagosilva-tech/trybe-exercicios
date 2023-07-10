5a1a8d82d37dae3001db6f37623864519a669ab5
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-07-10 19:26:18 -0300

    feat: exercicio08

--- Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio08.js
+++ Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio08.js
@@ -1,85 +1,129 @@
 const books = [
-    {
-      id: 1,
-      name: 'As Crônicas de Gelo e Fogo',
-      genre: 'Fantasia',
-      author: {
-        name: 'George R. R. Martin',
-        birthYear: 1948,
-      },
-      releaseYear: 1991,
+  {
+    id: 1,
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
+    author: {
+      name: "George R. R. Martin",
+      birthYear: 1948,
     },
-    {
-      id: 2,
-      name: 'O Senhor dos Anéis',
-      genre: 'Fantasia',
-      author: {
-        name: 'J. R. R. Tolkien',
-        birthYear: 1892,
-      },
-      releaseYear: 1954,
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
+    author: {
+      name: "J. R. R. Tolkien",
+      birthYear: 1892,
     },
-    {
-      id: 3,
-      name: 'Fundação',
-      genre: 'Ficção Científica',
-      author: {
-        name: 'Isaac Asimov',
-        birthYear: 1920,
-      },
-      releaseYear: 1951,
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: "Fundação",
+    genre: "Ficção Científica",
+    author: {
+      name: "Isaac Asimov",
+      birthYear: 1920,
     },
-    {
-      id: 4,
-      name: 'Duna',
-      genre: 'Ficção Científica',
-      author: {
-        name: 'Frank Herbert',
-        birthYear: 1920,
-      },
-      releaseYear: 1965,
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: "Duna",
+    genre: "Ficção Científica",
+    author: {
+      name: "Frank Herbert",
+      birthYear: 1920,
     },
-    {
-      id: 5,
-      name: 'A Coisa',
-      genre: 'Terror',
-      author: {
-        name: 'Stephen King',
-        birthYear: 1947,
-      },
-      releaseYear: 1986,
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: "A Coisa",
+    genre: "Terror",
+    author: {
+      name: "Stephen King",
+      birthYear: 1947,
     },
-    {
-      id: 6,
-      name: 'O Chamado de Cthulhu',
-      genre: 'Terror',
-      author: {
-        name: 'H. P. Lovecraft',
-        birthYear: 1890,
-      },
-      releaseYear: 1928,
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
+    author: {
+      name: "H. P. Lovecraft",
+      birthYear: 1890,
     },
-  ];
-  
-  // Adicione o código do exercício aqui:
+    releaseYear: 1928,
+  },
+];
+
+// Adicione o código do exercício aqui:
 
 //const expectedResult = 'Stephen King';
 const authorBornIn1947 = () => {
   // escreva aqui o seu código
-  const nameAuthor = books.find((book) => book.author.birthYear === 1947).author.name;
+  const nameAuthor = books.find((book) => book.author.birthYear === 1947).author
+    .name;
   return nameAuthor;
-}
+};
 
 //const expectedResult = 'Duna';
 const smallerName = () => {
-  let nameBook ;
+  let nameBook;
   // escreva aqui o seu código
-    books.forEach((book) => {
-        if (!nameBook || book.name.length < nameBook.length) {
-            nameBook = book.name;
-        }
-    })
+  books.forEach((book) => {
+    if (!nameBook || book.name.length < nameBook.length) {
+      nameBook = book.name;
+    }
+  });
   // Variável nameBook que receberá o valor do menor nome;
   return nameBook;
+};
+
+// const expectedResult = {
+//     author: {
+//       birthYear: 1948,
+//       name: 'George R. R. Martin',
+//     },
+//     genre: 'Fantasia',
+//     id: 1,
+//     name: 'As Crônicas de Gelo e Fogo',
+//     releaseYear: 1991,
+//   };
+const getNamedBook = () => {
+  // escreva seu código aqui
+  return books.find((book) => book.name.length === 26);
+};
+
+//   const expectedResult = false;
+function everyoneWasBornOnSecXX() {
+  // escreva seu código aqui
+  return books.every(
+    (book) => book.author.birthYear > 1900 && book.author.birthYear < 2001
+  );
 }
 
+//   const expectedResult = true;
+const someBookWasReleaseOnThe80s = () => {
+  // escreva seu código aqui
+  return books.some(
+    (book) => book.releaseYear > 1979 && book.releaseYear < 1991
+  );
+};
+
+//   const expectedResult = false;
+const authorUnique = () => {
+  // escreva seu código aqui
+  return books.every(
+    (book) =>
+      !books.some(
+        (bookSome) =>
+          (bookSome.author.birthYear === book.author.birthYear) &&
+          (bookSome.author.name !== book.author.name)
+      )
+  );
+};

0b04a4150c247017f1df80d18a2047fa89adc7ae
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-08-08 17:35:45 -0300

    feat: alert de error

--- Front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/api-de-super-herois/src/services/api.js
+++ Front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/api-de-super-herois/src/services/api.js
@@ -3,6 +3,12 @@ const API_BASE = "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api";
 export function getRandomHero() {
   const randomNumber = Math.floor(Math.random() * 731);
   return fetch(`${API_BASE}/id/${randomNumber}.json`)
-    .then((response) => response.json())
-    .then((data) => data);
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Erro ao buscar o SuperHeroi')
+      }
+      return response.json()
+    })
+    .then((data) => data)
+    .catch((error) => alert(error.message));
 }

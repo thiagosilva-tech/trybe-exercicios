51be757743fed76c713f70d9b6bfb9bffb2dc3c7
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-08-09 10:40:34 -0300

    feat: realizado todos os requisitos

--- Front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/select-pet/src/main.js
+++ Front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/select-pet/src/main.js
@@ -1,2 +1,25 @@
-import './style.css'
+import "./style.css";
+import { getRandomCat, getRandomDog, getSurprisePet } from "./services/api";
 
+const btnRandomCat = document.getElementById("btn-random-cat");
+const btnRandomDog = document.getElementById("btn-random-dog");
+const btnSurprise = document.getElementById("btn-surprise");
+const imgRandomPet = document.getElementById("img-random-pet");
+
+btnRandomCat.addEventListener("click", () => {
+  getRandomCat().then((cat) => {
+    imgRandomPet.src = cat[0].url;
+  });
+});
+
+btnRandomDog.addEventListener("click", () => {
+  getRandomDog().then((dog) => {
+    imgRandomPet.src = dog.message;
+  });
+});
+
+btnSurprise.addEventListener("click", () => {
+  getSurprisePet().then((pet) =>
+    pet[0] ? (imgRandomPet.src = pet[0].url) : (imgRandomPet.src = pet.message)
+  );
+});

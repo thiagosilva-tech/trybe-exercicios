2cf9697046ce0dc7a0a59ed8ed4c03987b095e99
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-12-01 14:11:19 -0300

    feat: corrigindo API

--- Back-end/introducao-ao-desenvolvimento-web-com-node-js/dia-03-node-js-testes-de-integracao/cacau-trybe/src/app.js
+++ Back-end/introducao-ao-desenvolvimento-web-com-node-js/dia-03-node-js-testes-de-integracao/cacau-trybe/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cacauTrybe = require('./cacauTrybe');
   
 const app = express();
 app.use(express.json());
@@ -8,28 +9,28 @@ app.get('/chocolates', async (req, res) => {
     res.status(200).json({ chocolates }); 
 });
 
-app.get('/chocolates/:id', (req, res) => {
-    const { id } = req.params;
+// app.get('/chocolates/:id', (req, res) => {
+//     const { id } = req.params;
 
-    const findChocolate = chocolates.find((chocolate) => chocolate.id === Number(id));
+//     const findChocolate = chocolates.find((chocolate) => chocolate.id === Number(id));
 
-    if (!findChocolate) {
-        return res.status(404).json({ message: 'Chocolate not found' });
-    }
+//     if (!findChocolate) {
+//         return res.status(404).json({ message: 'Chocolate not found' });
+//     }
 
-    res.status(200).json({ findChocolate });
-});
+//     res.status(200).json({ findChocolate });
+// });
 
-app.get('/chocolates/brand/:brandId', (req, res) => {
-    const { brandId } = req.params;
+// app.get('/chocolates/brand/:brandId', (req, res) => {
+//     const { brandId } = req.params;
 
-    const findBrand = chocolates.find((chocolate) => chocolate.brandId === Number(brandId));
+//     const findBrand = chocolates.find((chocolate) => chocolate.brandId === Number(brandId));
 
-    if (!findBrand) {
-        return res.status(404).json({ message: 'Brand not found' });
-    }
+//     if (!findBrand) {
+//         return res.status(404).json({ message: 'Brand not found' });
+//     }
 
-    res.status(200).json({ findBrand });
-});
+//     res.status(200).json({ findBrand });
+// });
 
 module.exports = app;

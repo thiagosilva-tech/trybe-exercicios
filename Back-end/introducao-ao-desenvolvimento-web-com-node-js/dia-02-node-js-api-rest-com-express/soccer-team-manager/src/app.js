00458f8342a2a9036fdcc6422dbc8b190d301de2
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-12-04 10:53:44 -0300

    fix: eslint

--- Back-end/introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/soccer-team-manager/src/app.js
+++ Back-end/introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/soccer-team-manager/src/app.js
@@ -1,11 +1,6 @@
-// src/app.js
-
 const express = require('express');
-
 const validateTeam = require('./middlewares/validateTeam');
-
 const existingId = require('./middlewares/existingId');
-
 const teams = require('./utils/teams');
 
 const app = express();
@@ -19,31 +14,31 @@ app.get('/teams', (req, res) => res.json(teams));
 app.get('/teams/:id', existingId, (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find((t) => t.id === id);
-    res.json(team);
+  res.json(team);
 });
 
 app.post('/teams', validateTeam, (req, res) => {
-    const team = { id: nextId, ...req.body };
-    teams.push(team);
-    nextId += 1;
-    res.status(201).json(team);
-  });
+  const team = { id: nextId, ...req.body };
+  teams.push(team);
+  nextId += 1;
+  res.status(201).json(team);
+});
 
 app.put('/teams/:id', existingId, validateTeam, (req, res) => {
-    const id = Number(req.params.id);  
-    const team = teams.find((t) => t.id === id);  
-    const index = teams.indexOf(team);  
-    const updated = { id, ...req.body };  
-    teams.splice(index, 1, updated);  
-    res.status(201).json(updated);
+  const id = Number(req.params.id);
+  const team = teams.find((t) => t.id === id);
+  const index = teams.indexOf(team);
+  const updated = { id, ...req.body };
+  teams.splice(index, 1, updated);
+  res.status(201).json(updated);
 });
 
 app.delete('/teams/:id', existingId, (req, res) => {
-    const id = Number(req.params.id);
-    const team = teams.find((t) => t.id === id);
-    const index = teams.indexOf(team);
-    teams.splice(index, 1);
-    res.sendStatus(204);
+  const id = Number(req.params.id);
+  const team = teams.find((t) => t.id === id);
+  const index = teams.indexOf(team);
+  teams.splice(index, 1);
+  res.sendStatus(204);
 });
 
 module.exports = app;

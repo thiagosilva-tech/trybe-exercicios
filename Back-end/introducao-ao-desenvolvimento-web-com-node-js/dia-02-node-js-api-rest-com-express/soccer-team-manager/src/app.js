fdc2daa633f6b942fded5cc054dedd3cc092e5c9
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-12-04 13:36:25 -0300

    feat: Router middleware

--- Back-end/introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/soccer-team-manager/src/app.js
+++ Back-end/introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/soccer-team-manager/src/app.js
@@ -1,72 +1,25 @@
+// src/app.js
+
 const express = require('express');
 require('express-async-errors');
 const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
-const validateTeam = require('./middlewares/validateTeam');
-const existingId = require('./middlewares/existingId');
-const teams = require('./utils/teams');
-
-const apiCredentials = require('./middlewares/apiCredentials');
+// require no nosso novo router
+const teamsRouter = require('./routes/teamsRouter');
 
 const app = express();
-
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 15 * 60 * 1000,
-  message: 'Muitas requisições originadas desta IP',
-});
-
-app.use(limiter);
-
-app.use(helmet());
-let nextId = 3;
 app.use(morgan('dev'));
-app.use(cors());
-
+app.use(express.static('/images'));
 app.use(express.json());
-app.use(express.static('images'));
-app.use(apiCredentials);
+// monta o router na rota /teams (1)
+app.use('/teams', teamsRouter);
 
-app.get('/teams', (req, res) => res.json(teams));
-
-app.get('/teams/:id', existingId, (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find((t) => t.id === id);
-  res.json(team);
+app.use((err, _req, _res, next) => {
+  console.error(err.stack);
+  next(err);
 });
 
-app.post('/teams', validateTeam, (req, res) => {
-  if (
-    !req.teams.teams.includes(req.body.sigla)
-    && teams.every((team) => team.sigla !== req.body.sigla)
-  ) {
-    return res.status(422).json({ message: 'Já existe um time com essa sigla' });
-  }
-  const team = { id: nextId, ...req.body };
-  teams.push(team);
-  nextId += 1;
-  res.status(201).json(team);
+app.use((err, _req, res, _next) => {
+  res.status(500).json({ message: `Algo deu errado! Mensagem: ${err.message}` });
 });
-
-app.put('/teams/:id', existingId, validateTeam, (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find((t) => t.id === id);
-  const index = teams.indexOf(team);
-  const updated = { id, ...req.body };
-  teams.splice(index, 1, updated);
-  res.status(201).json(updated);
-});
-
-app.delete('/teams/:id', existingId, (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find((t) => t.id === id);
-  const index = teams.indexOf(team);
-  teams.splice(index, 1);
-  res.sendStatus(204);
-});
-
-app.use((req, res) => res.sendStatus(404));
 
 module.exports = app;

daf9145321e32c781e96a931884a5502e758a63b
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-12-04 11:29:17 -0300

    feat: Liberando acesso ao frontend com cors

--- Back-end/introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/soccer-team-manager/src/app.js
+++ Back-end/introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/soccer-team-manager/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('express-async-errors');
 const morgan = require('morgan');
+const cors = require('cors');
 const validateTeam = require('./middlewares/validateTeam');
 const existingId = require('./middlewares/existingId');
 const teams = require('./utils/teams');
@@ -10,6 +11,7 @@ const apiCredentials = require('./middlewares/apiCredentials');
 const app = express();
 let nextId = 3;
 app.use(morgan('dev'));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.static('./images'));

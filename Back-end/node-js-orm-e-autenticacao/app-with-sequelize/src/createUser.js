259f36caff826610a8ecb0bc5e85e0c8c79fe120
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2024-01-24 11:14:06 -0300

    feat: model

---
+++ Back-end/node-js-orm-e-autenticacao/app-with-sequelize/src/createUser.js
@@ -0,0 +1,12 @@
+// src/createUser.js
+
+const { User } = require('./models');
+
+(async () => {
+  const user = await User.create({
+    fullName: 'Leonardo',
+    email: 'leonardo@mail.com',
+    phoneNum: '911221133',
+  });
+  console.log(user);
+})();
